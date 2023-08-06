0191ad0dd8a1bf0868073273c7146116962fc01e
Richard <richard232manansala@gmail.com>
2023-08-06 08:09:16 +0800

    Made getProjects get revalidated evry 3000s

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,9 @@ export async function getProjects(): Promise<Project[]> {
   const rootUrl =
     "https://raw.githubusercontent.com/fireWizard23/profile_projects_list/main";
 
-  const projectsResponse = await fetch(`${rootUrl}/list.json`);
+  const projectsResponse = await fetch(`${rootUrl}/list.json`, {
+    next: { revalidate: 3000 },
+  });
 
   const projects: Project[] = await projectsResponse.json();
 
